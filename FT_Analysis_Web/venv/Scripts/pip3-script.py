d0ea3d4b35f2880841b7533edc463a575056b699
jingyuantan <jingyuantan1028@gmail.com>
2019-10-14 16:58:55 +0100

    1st commit

---
+++ FT_Analysis_Web/venv/Scripts/pip3-script.py
@@ -0,0 +1,12 @@
+#!E:\Users\User\PycharmProjects\Network-Analysis-of-the-Global-Football-Transfer-Market\FT_Analysis_Web\venv\Scripts\python.exe
+# EASY-INSTALL-ENTRY-SCRIPT: 'pip==19.0.3','console_scripts','pip3'
+__requires__ = 'pip==19.0.3'
+import re
+import sys
+from pkg_resources import load_entry_point
+
+if __name__ == '__main__':
+    sys.argv[0] = re.sub(r'(-script\.pyw?|\.exe)?$', '', sys.argv[0])
+    sys.exit(
+        load_entry_point('pip==19.0.3', 'console_scripts', 'pip3')()
+    )
