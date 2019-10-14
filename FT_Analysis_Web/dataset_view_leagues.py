a9ed51f8357e86970d81b2465ba42a0cac4e407d
jingyuantan <jingyuantan1028@gmail.com>
2019-10-14 20:26:39 +0100

    data cleaning (League)

--- FT_Analysis_Web/dataset_view_leagues.py
+++ FT_Analysis_Web/dataset_view_leagues.py
@@ -6,28 +6,26 @@ arr = []
 exist = False
 
 for i in range(len(df)):
-    league_from = df["from"][i]["league"]
-    league_to = df["to"][i]["league"]
+    league_from = df["from"][i]["leagueId"]
+    league_to = df["to"][i]["leagueId"]
 
-    if league_from == "":
-        continue
-    arr.append(league_from)
+    if league_from != "":
+        arr.append(league_from)
 
-    if league_to == '':
-        continue
-    arr.append(league_to)
+    if league_to != "":
+        arr.append(league_to)
 
 my_list = list(dict.fromkeys(arr))
 my_list.sort()
 print(my_list)
 print(len(my_list))
 
-result_file = open("dataset/league_list.csv", "w", encoding='utf-8')
+result_file = open("dataset/leagueId_list.csv", "w", encoding='utf-8')
 for r in my_list:
     result_file.write(r + "\n")
 result_file.close()
 
-result_file = open("dataset/league_list.txt", "w", encoding='utf-8')
+result_file = open("dataset/leagueId_list.txt", "w", encoding='utf-8')
 for r in my_list:
     result_file.write(r + "\n")
 result_file.close()
