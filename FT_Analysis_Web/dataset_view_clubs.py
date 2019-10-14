6a2cea1755b227d892e5e8327c9d7df2834d7e94
jingyuantan <jingyuantan1028@gmail.com>
2019-10-14 20:36:43 +0100

    data cleaning (clubs)

--- FT_Analysis_Web/dataset_view_clubs.py
+++ FT_Analysis_Web/dataset_view_clubs.py
@@ -1,33 +1,27 @@
 import pandas as pd
 
 df = pd.read_json('dataset/transfers-2018_19-20191004.json', lines=True)
-count = 0
 arr = []
-exist = False
 
 for i in range(len(df)):
     club_from = df["from"][i]["name"]
     club_to = df["to"][i]["name"]
 
     # clean club_from data
-    if club_from == "":
-        continue
-    if club_from[:1] == " ":
-        club_from = club_from[1:]
-    if club_from[-3:-2] == 'U':
-        club_from = club_from[:-4]
-
-    arr.append(club_from)
+    if club_from != "":
+        if club_from[:1] == " ":
+            club_from = club_from[1:]
+        if club_from[-3:-2] == 'U':
+            club_from = club_from[:-4]
+        arr.append(club_from)
 
     # clean club_to data
-    if club_to == '':
-        continue
-    if club_to[:1] == " ":
-        club_to = club_to[1:]
-    if club_to[-3:-2] == 'U':
-        club_to = club_to[:-4]
-
-    arr.append(club_to)
+    if club_to != "":
+        if club_to[:1] == " ":
+            club_to = club_to[1:]
+        if club_to[-3:-2] == 'U':
+            club_to = club_to[:-4]
+        arr.append(club_to)
 
 my_list = list(dict.fromkeys(arr))
 my_list.sort()
