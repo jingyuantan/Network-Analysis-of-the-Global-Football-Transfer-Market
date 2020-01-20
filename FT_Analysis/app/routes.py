2a978711e371d98ef26094f3a2d26eb4dc752c42
jingyuantan <jingyuantan1028@gmail.com>
2020-01-20 10:30:29 +0000

    first interactive full network graph

--- FT_Analysis/app/routes.py
+++ FT_Analysis/app/routes.py
@@ -1,7 +1,9 @@
 from flask import Flask, render_template, request
 import plotly
 import plotly.graph_objs as go
-
+import networkx as nx
+from app import db
+from app.models import Player, League, Club, Transfer
 import pandas as pd
 import numpy as np
 import json
@@ -24,16 +26,106 @@ def explore():
 
 def create_plot(feature):
     if feature == 'Bar':
-        N = 40
+        """N = 40
         x = np.linspace(0, 1, N)
         y = np.random.randn(N)
-        df = pd.DataFrame({'x': x, 'y': y}) # creating a sample dataframe
+        df = pd.DataFrame({'x': x, 'y': y})  # creating a sample dataframe
         data = [
             go.Bar(
-                x=df['x'], # assign x as the dataframe column 'x'
+                x=df['x'],  # assign x as the dataframe column 'x'
                 y=df['y']
             )
-        ]
+        ]"""
+
+        G = nx.DiGraph()
+        players = Player.query.all()
+        leagues = League.query.all()
+        transfers = Transfer.query.all()
+
+        for transfer in transfers:
+            clubFrom = Club.query.filter_by(id=transfer.fromId).first()
+            clubTo = Club.query.filter_by(id=transfer.toId).first()
+            G.add_node(clubFrom.name)
+            G.add_node(clubTo.name)
+            G.add_edge(clubFrom.name, clubTo.name)
+
+        pos = nx.spring_layout(G, k=0.5, iterations=50)
+        for n,p in pos.items():
+            G.nodes[n]['pos'] = p
+        edge_x = []
+        edge_y = []
+        for edge in G.edges():
+            x0, y0 = G.nodes[edge[0]]['pos']
+            x1, y1 = G.nodes[edge[1]]['pos']
+            edge_x.append(x0)
+            edge_x.append(x1)
+            edge_x.append(None)
+            edge_y.append(y0)
+            edge_y.append(y1)
+            edge_y.append(None)
+
+        edge_trace = go.Scatter(
+            x=edge_x, y=edge_y,
+            line=dict(width=0.5, color='#888'),
+            hoverinfo='none',
+            mode='lines')
+
+        node_x = []
+        node_y = []
+        for node in G.nodes():
+            x, y = G.nodes[node]['pos']
+            node_x.append(x)
+            node_y.append(y)
+
+        node_trace = go.Scatter(
+            x=node_x, y=node_y,
+            mode='markers',
+            hoverinfo='text',
+            marker=dict(
+                showscale=True,
+                # colorscale options
+                # 'Greys' | 'YlGnBu' | 'Greens' | 'YlOrRd' | 'Bluered' | 'RdBu' |
+                # 'Reds' | 'Blues' | 'Picnic' | 'Rainbow' | 'Portland' | 'Jet' |
+                # 'Hot' | 'Blackbody' | 'Earth' | 'Electric' | 'Viridis' |
+                colorscale='YlGnBu',
+                reversescale=True,
+                color=[],
+                size=10,
+                colorbar=dict(
+                    thickness=15,
+                    title='Node Connections',
+                    xanchor='left',
+                    titleside='right'
+                ),
+                line_width=2))
+
+        node_adjacencies = []
+        node_text = []
+        for node, adjacencies in enumerate(G.adjacency()):
+            node_adjacencies.append(len(adjacencies[1]))
+            node_text.append('# of connections: ' + str(len(adjacencies[1])))
+
+        node_trace.marker.color = node_adjacencies
+        node_trace.text = node_text
+
+        fig = go.Figure(data=[edge_trace, node_trace],
+                        layout=go.Layout(
+                            title='<br>Network graph made with Python',
+                            titlefont_size=16,
+                            showlegend=False,
+                            hovermode='closest',
+                            margin=dict(b=20, l=5, r=5, t=40),
+                            annotations=[dict(
+                                text="Python code: <a href='https://plot.ly/ipython-notebooks/network-graphs/'> https://plot.ly/ipython-notebooks/network-graphs/</a>",
+                                showarrow=False,
+                                xref="paper", yref="paper",
+                                x=0.005, y=-0.002)],
+                            xaxis=dict(showgrid=False, zeroline=False, showticklabels=False),
+                            yaxis=dict(showgrid=False, zeroline=False, showticklabels=False))
+                        )
+        graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
+        return graphJSON
+
     else:
         N = 1000
         random_x = np.random.randn(N)
--- FT_Analysis/test.py
+++ FT_Analysis/test.py
@@ -1,6 +1,7 @@
 from sqlalchemy import *
 from app import db
-from app.models import Player
+from app.models import Player, Transfer, Club
+transfers = Transfer.query.all()
 import re
 """
 test = re.compile('U[0-9][0-9]')
@@ -15,3 +16,15 @@ else:
     print("qwer")
 """
 
+import plotly.graph_objects as go
+
+import networkx as nx
+
+G = nx.random_geometric_graph(200, 0.125)
+edge_x = []
+edge_y = []
+for edge in G.edges():
+    x0, y0 = G.nodes[edge[0]]['pos']
+    x1, y1 = G.nodes[edge[1]]['pos']
+    print(G.nodes[edge[0]]['pos'])
+    print(G.nodes[edge[1]]['pos'])
